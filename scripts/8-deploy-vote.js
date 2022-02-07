ea225237db33d16d63dbb6fd74371a8dd4ce8484
InfiAB <tmtg360@gmail.com>
2022-02-07 10:25:18 +0530

    s3p3 half

--- scripts/8-deploy-vote.js
+++ scripts/8-deploy-vote.js
@@ -1,37 +1,41 @@
-
 import sdk from "./1-initialize-sdk.js";
 
 // Grab the app module address.
 const appModule = sdk.getAppModule(
-    "0x5EcD75268029D1427dBa5c8e853F37f4aAb23B9d",
+  "0x5EcD75268029D1427dBa5c8e853F37f4aAb23B9d",
 );
 
 (async () => {
-    try {
-        const voteModule = await appModule.deployVoteModule({
-            // GIve your governance contract a name.
-            name: "znmdArjunDAO's frist Proposal",
+  try {
+    const voteModule = await appModule.deployVoteModule({
+      // Give your governance contract a name.
+      name: "znmdArjunDAO's frist Proposal",
 
-            // This is the location of our governance token our ERC-20 contract!
-            votingTokenAddress: "0x14997B453d505144e7Ad11AC69544FA84c9ADcE2",
+      // This is the location of our governance token, our ERC-20 contract!
+      votingTokenAddress: "0x14997B453d505144e7Ad11AC69544FA84c9ADcE2",
 
-            // After a proposal is created, when can members start voting ?
-            // For now, we set this to immediately.
-            proposalStartWaitTimeInSeconds: 0,
+      // After a proposal is created, when can members start voting?
+      // For now, we set this to immediately.
+      proposalStartWaitTimeInSeconds: 0,
 
-            // How long do members have to vote on a proposal when it's created.
-            // Here we set it ro 24 hours (86400 seconds)
-            proposalVotingTimeInSeconds: 24 * 60 * 60,
-            
-            // Will explain more below.
-            votingQuorumFraction: 0,
+      // How long do members have to vote on a proposal when it's created?
+      // Here, we set it to 24 hours (86400 seconds)
+      proposalVotingTimeInSeconds: 24 * 60 * 60,
 
-            // What's the minimum # of tokens a user needs to be allowed to create a proposal?
-            // I ser it to 0. Meaning no tokens are required for a user to create a proposal.
-            minimumNNumberOfTokensNeededToPropose: "0",
-            });
-            console.log("✅ Successfully deployed vote module, address: ", voteModule.address)
-    } catch (error) {
-        console.log("Failed to deploy voite module", error);
-    }
+      // Will explain more below.
+      votingQuorumFraction: 0,
+
+      // What's the minimum # of tokens a user needs to be allowed to create a proposal?
+      // I set it to 0. Meaning no tokens are required for a user to be allowed to
+      // create a proposal.
+      minimumNumberOfTokensNeededToPropose: "0",
+    });
+
+    console.log(
+      "✅ Successfully deployed vote module, address:",
+      voteModule.address,
+    );
+  } catch (err) {
+    console.error("Failed to deploy vote module", err);
+  }
 })();
